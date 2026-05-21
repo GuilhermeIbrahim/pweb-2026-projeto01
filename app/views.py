bd14a509101d2f27ebbc7683b5410c80a0da0129
Ibrahim Guilherme <guilhermeibrahim08@gmail.com>
2026-05-21 11:54:24 -0300

    Feito o index.html de acordo com os requisitos

--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, "app/index.html")
+    context = {"resumo": "Percy Jackson, um semideus de apenas 12 anos, aceita seus recém-descobertos poderes divinos quando o senhor dos deuses, Zeus, o acusa de roubar seu raio. Com a ajuda de um amigo, Percy precisa restaurar a ordem no Olimpo.", "autor": "Rick Riordan", "titulo": "Percy Jackson e os Olimpianos"}
+    return render(request, "app/index.html", context)
 
 def elenco(request):
     return render(request, "app/elenco.html")
